93a376681b7d5f09d6dff014973b094a16e97006
Kevin Davis <thekidnamedkd@gmail.com>
2020-07-29 22:32:37 -0700

    tighten up syntax and indents. align both models to begin UI.

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,13 +1,14 @@
 using System;
-using System.Collections.Generic;
+using PierresBakery.Models;
 
-namespace PierresBakery.Models
+namespace PierresBakery
 {
   public class Program
   {
     public static void Main()
     {
-      
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("Today we have many baked goods. We recommend our bread and our pastries.");
     }
   }
 }
