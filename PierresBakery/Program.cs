196670345d25588528ef1912b53e80787e567427
Kevin Davis <thekidnamedkd@gmail.com>
2020-07-29 22:57:10 -0700

    start to build UI via Program.

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -8,7 +8,18 @@ namespace PierresBakery
     public static void Main()
     {
       Console.WriteLine("Welcome to Pierre's Bakery!");
-      Console.WriteLine("Today we have many baked goods. We recommend our bread and our pastries.");
+      Console.WriteLine("- - -");
+      Console.WriteLine("Today we have many baked goods. We recommend our bread and our pastries!");
+      Console.WriteLine("We have baguettes for 5 and pastries for 2.");
+      Console.WriteLine("- - -");
+      Console.WriteLine("If you're looking for deal:");
+      Console.WriteLine("We have buy two, get one on baguettes...");
+      Console.WriteLine("And three pastries for 5!");
+      Console.WriteLine("- - -");
+      Console.WriteLine("How many baguettes would you like?");
+      int userBread = int.Parse(Console.ReadLine());
+      Console.WriteLine("How many pastries would you like?");
+      int userPastry = int.Parse(Console.ReadLine());
     }
   }
 }
